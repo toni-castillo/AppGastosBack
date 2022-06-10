10cc0dba3934fc45aa67d85ee8313ae101529ac0
Toni Castillo <castillolozano@gmail.com>
2022-06-10 20:00:52 +0200

    Create the base of the application

---
+++ dbConfig.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2'); // Importa el módulo mysql2 para conectar con la base de datos
+
+// console.log('Configuro la base de datos');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
+
+global.db = pool;
