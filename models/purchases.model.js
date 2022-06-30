58708be3d84f9014ff61e946ff8501e0cd2e7bda
Toni Castillo <castillolozano@gmail.com>
2022-06-30 19:04:43 +0200

    Update purchases

--- models/purchases.model.js
+++ models/purchases.model.js
@@ -1,7 +1,7 @@
 const { executeQuery, executeQueryOne } = require("../helpers")
 
 const create = ({ date_request, name, surname, department, project_code, reason, date_expense, product_link, provider, units, amount, type }) => {
-  return executeQuery('INSERT INTO appgastos.expenses (date_request, name, surname, department, project_code, reason, date_expense, product_link, provider, units, amount, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [date_request, name, surname, department, project_code, reason, STR_TO_DATE(date_expense, "%Y-%m-%d %H:%i:%s"), product_link, provider, units, amount, "purchase"]);
+  return executeQuery('INSERT INTO appgastos.expenses (date_request, name, surname, department, project_code, reason, date_expense, product_link, provider, units, amount, general_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [date_request, name, surname, department, project_code, reason, date_expense, product_link, provider, units, amount, "purchase"]);
 }
 
 module.exports = {
