e0a41e5d062a21ef780e99fba30bad1c305a91f5
Toni Castillo <castillolozano@gmail.com>
2022-07-06 21:24:59 +0200

    Update ddbb files

--- models/purchases.model.js
+++ models/purchases.model.js
@@ -20,6 +20,12 @@ const deleteOne = (purchaseId) => {
   return executeQuery('DELETE FROM appgastos.expenses WHERE id = ?', [purchaseId]);
 }
 
+
+// const create = ({ date_request, name, surname, department, project_code, reason, date_expense, product_link, provider, units, amount, general_type }) => {
+//   return executeQuery('INSERT INTO appgastos.expenses (date_request, name, surname, department, project_code, reason, date_expense, product_link, provider, units, amount, general_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [date_request, name, surname, department, project_code, reason, date_expense, product_link, provider, units, amount, "purchase"]);
+// }
+
+
 module.exports = {
   create, getAll, getOne, update, deleteOne
 }
